ee65ec1d4ed527ba201722e12243e54c21001225
Mameng06 <meifam0623@gmail.com>
2025-10-22 18:32:57 -0700

    added manual conversion rgb to lab

--- services/ColorDetectorInference.ts
+++ services/ColorDetectorInference.ts
@@ -34,8 +34,31 @@ export async function inferColorFromRGB(rgb: { r: number; g: number; b: number }
     }
 
     console.log("ColorDetectorInference: Using TensorFlow Lite model for inference");
-    const Color = require('colorjs.io');
-    const detected = new (Color as any)('srgb', [sr, sg, sb]).to('lab');
+    let detected;
+    try {
+      const Color = require('colorjs.io');
+      detected = new Color('srgb', [sr, sg, sb]).to('lab');
+    } catch (colorError) {
+      console.log("ColorDetectorInference: Color.js failed, using manual RGB to LAB conversion");
+      // Manual RGB to LAB conversion as fallback
+      const r = sr > 0.04045 ? Math.pow((sr + 0.055) / 1.055, 2.4) : sr / 12.92;
+      const g = sg > 0.04045 ? Math.pow((sg + 0.055) / 1.055, 2.4) : sg / 12.92;
+      const b = sb > 0.04045 ? Math.pow((sb + 0.055) / 1.055, 2.4) : sb / 12.92;
+      
+      const x = r * 0.4124564 + g * 0.3575761 + b * 0.1804375;
+      const y = r * 0.2126729 + g * 0.7151522 + b * 0.0721750;
+      const z = r * 0.0193339 + g * 0.1191920 + b * 0.9503041;
+      
+      const fx = x > 0.008856 ? Math.pow(x, 1/3) : (7.787 * x + 16/116);
+      const fy = y > 0.008856 ? Math.pow(y, 1/3) : (7.787 * y + 16/116);
+      const fz = z > 0.008856 ? Math.pow(z, 1/3) : (7.787 * z + 16/116);
+      
+      const l = 116 * fy - 16;
+      const a = 500 * (fx - fy);
+      const b_lab = 200 * (fy - fz);
+      
+      detected = { coords: [l, a, b_lab] };
+    }
     const l = detected.coords[0];
     const a = detected.coords[1];
     const b = detected.coords[2];
